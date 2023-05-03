d9abff1c1225da893002ecdedfe173e878e034b1
bread99 <puppy7962@gmail.com>
2023-05-04 04:19:52 +0900

    npm?

---
+++ client/src/components/Buttons.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CommonButton = styled.button`
+  background-color: ${(props) => props.bgColor || "var(--powder-100)"};
+  margin: 4px;
+  padding: 10px;
+  box-shadow: inset 0 1px 0 0 hsl(0deg 0% 100% / 40%);
+  border-radius: 3px;
+  color: ${(props) => props.color || "var(--powder-700)"};
+  font-size: 13px;
+  border: 1px solid ${(props) => props.border || "var(--blue-600)"};
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+`;
