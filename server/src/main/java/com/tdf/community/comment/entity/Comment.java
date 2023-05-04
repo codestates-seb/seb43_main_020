8c1a8d9e906d940d8fa19929cffcbf601ef300d1
chipmunk-dev <devquokkajeong@gmail.com>
2023-05-04 10:43:59 +0900

    #9 - 댓글 엔티티 설계

---
+++ server/src/main/java/com/tdf/community/comment/entity/Comment.java
@@ -0,0 +1,15 @@
+package com.tdf.community.comment.entity;
+
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private Long commentId;
+    private Article article; // 게시글 (ID)
+    private String content; // 본문
+
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
+}
