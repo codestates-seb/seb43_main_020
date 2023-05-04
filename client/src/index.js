2e984c167a72e0feaa2c875f613de3c7ba0461a2
nullpp00 <nullpp00@gmail.com>
2023-05-04 14:28:28 +0900

    Main Page

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import GlobalStyle from "./GlobalStyle";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <GlobalStyle /> */}
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
