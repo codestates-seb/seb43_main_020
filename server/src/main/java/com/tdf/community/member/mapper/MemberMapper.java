d63012030e01e396f85a443913affc33b12f1c58
seoeunjeong <2juillet.acc@gmail.com>
2023-05-05 19:18:22 +0900

    #2 - memberdomain로직작성

---
+++ server/src/main/java/com/tdf/community/member/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package com.tdf.community.member.mapper;
+
+import com.tdf.community.member.dto.MemberDto;
+import com.tdf.community.member.entity.Member;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+
+    Member memberPostDtoToMember(MemberDto.Post requestBody);
+
+    Member memberPatchDtoToMember(MemberDto.Patch requestBody);
+    MemberDto.Response memberToMemberResponseDto(Member member);
+
+    List<MemberDto.Response> membersTomemberResponseDtos(List<Member> findMembers);
+}
