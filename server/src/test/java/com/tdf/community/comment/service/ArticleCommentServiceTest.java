b192bc3cac63d689917a56e676f9fbc05e3b7746
chipmunk-dev <devquokkajeong@gmail.com>
2023-05-08 04:48:02 +0900

    #16 - comment test 수정

--- server/src/test/java/com/tdf/community/comment/service/ArticleCommentServiceTest.java
+++ server/src/test/java/com/tdf/community/comment/service/ArticleCommentServiceTest.java
@@ -1,23 +1,26 @@
 package com.tdf.community.comment.service;
 
-import com.tdf.community.article.dto.ArticleUpdateDto;
 import com.tdf.community.article.entity.Article;
 import com.tdf.community.article.repository.ArticleRepository;
 import com.tdf.community.comment.dto.ArticleCommentDto;
-import com.tdf.community.comment.dto.ArticleCommentUpdateDto;
 import com.tdf.community.comment.entity.ArticleComment;
 import com.tdf.community.comment.repository.ArticleCommentRepository;
+import com.tdf.community.member.dto.MemberDto;
+import com.tdf.community.member.entity.Member;
+import com.tdf.community.member.mapper.MemberMapper;
+import com.tdf.community.member.mapper.MemberMapperImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,57 +29,151 @@ import static org.mockito.BDDMockito.*;
 @DisplayName("비즈니스 로직 - 댓글")
 @ExtendWith(MockitoExtension.class)
 class ArticleCommentServiceTest {
-
-    @InjectMocks private ArticleCommentService articleCommentService;
-
+    @InjectMocks private ArticleCommentService sut;
     @Mock private ArticleRepository articleRepository;
     @Mock private ArticleCommentRepository articleCommentRepository;
+    private MemberMapper memberMapper = Mappers.getMapper(MemberMapper.class);
 
-    @DisplayName("게시글 Id로 게시글 댓글 리스트를 가져온다.")
+    @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
-    void getCommentListByArticleId() {
+    void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
+        // Given
         Long articleId = 1L;
-        Optional<Article> articleComment = Optional.of(Article.of("title", "content", "#pet"));
+        ArticleComment expected = createArticleComment("content");
+        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(expected));
 
-        given(articleRepository.findById(articleId)).willReturn(articleComment);
+        // When
+        List<ArticleCommentDto> actual = sut.searchArticleComment(articleId);
 
-        List<ArticleCommentDto> comments = articleCommentService.searchArticleComment(1L);
-
-        assertThat(comments).isNotNull();
-        then(articleRepository).should().findById(articleId);
+        // Then
+        assertThat(actual)
+                .hasSize(1)
+                .first().hasFieldOrPropertyWithValue("content", expected.getContent());
+        then(articleCommentRepository).should().findByArticle_Id(articleId);
     }
 
     @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
     @Test
-    void createComment() {
-        ArticleCommentDto dto = ArticleCommentDto.of("Test Content", LocalDateTime.now(), "writer", LocalDateTime.now(), "udpater");
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.getArticleId())).willReturn(createArticle());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
-        articleCommentService.saveComment(dto);
+        // When
+        sut.saveComment(dto);
 
+        // Then
+        then(articleRepository).should().getReferenceById(dto.getArticleId());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
-    @DisplayName("수정된 댓글 정보를 입력하면, 댓글을 수정한다.")
+    @DisplayName("댓글 저장을 시도했는데 맞는 게시글이 없으면, 경고 로그를 찍고 아무것도 안 한다.")
     @Test
-    void updateComment() {
-        ArticleCommentUpdateDto dto = ArticleCommentUpdateDto.of("update content");
-        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
+    void givenNonexistentArticle_whenSavingArticleComment_thenLogsSituationAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.getArticleId())).willThrow(EntityNotFoundException.class);
 
-        articleCommentService.updateComment(1L, dto);
+        // When
+        sut.saveComment(dto);
 
-        then(articleCommentRepository).should().save(any(ArticleComment.class));
+        // Then
+        then(articleRepository).should().getReferenceById(dto.getArticleId());
+        then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
-    @DisplayName("댓글을 삭제한다.")
+    @DisplayName("댓글 정보를 입력하면, 댓글을 수정한다.")
     @Test
-    void deleteComment() {
-        willDoNothing().given(articleCommentRepository).delete(any(ArticleComment.class));
+    void givenArticleCommentInfo_whenUpdatingArticleComment_thenUpdatesArticleComment() {
+        // Given
+        String oldContent = "content";
+        String updatedContent = "댓글";
+        ArticleComment articleComment = createArticleComment(oldContent);
+        ArticleCommentDto dto = createArticleCommentDto(updatedContent);
+        given(articleCommentRepository.getReferenceById(dto.getArticleId())).willReturn(articleComment);
 
-        articleCommentService.deleteComment(1L);
+        // When
+        sut.updateComment(dto);
 
-        then(articleCommentRepository).should().delete(any(ArticleComment.class));
+        // Then
+        assertThat(articleComment.getContent())
+                .isNotEqualTo(oldContent)
+                .isEqualTo(updatedContent);
+        then(articleCommentRepository).should().getReferenceById(dto.getArticleId());
     }
 
+    @DisplayName("없는 댓글 정보를 수정하려고 하면, 경고 로그를 찍고 아무 것도 안 한다.")
+    @Test
+    void givenNonexistentArticleComment_whenUpdatingArticleComment_thenLogsWarningAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleCommentRepository.getReferenceById(dto.getArticleId())).willThrow(EntityNotFoundException.class);
+
+        // When
+        sut.updateComment(dto);
+
+        // Then
+        then(articleCommentRepository).should().getReferenceById(dto.getArticleId());
+    }
+
+    @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
+    @Test
+    void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
+        // Given
+        Long articleCommentId = 1L;
+        willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
+
+        // When
+        sut.deleteComment(articleCommentId);
+
+        // Then
+        then(articleCommentRepository).should().deleteById(articleCommentId);
+    }
+
+
+    private ArticleCommentDto createArticleCommentDto(String content) {
+        return ArticleCommentDto.of(
+                1L,
+                1L,
+                memberMapper.memberToMemberResponseDto(createUserAccount()),
+                content,
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
+    private MemberDto.Response createUserAccountDto() {
+        return new MemberDto.Response(1L, "Test@test.com", "test User", 31, "male", "abc", Member.MemberStatus.MEMBER_JOIN, ZonedDateTime.now());
+    }
+
+    private ArticleComment createArticleComment(String content) {
+        return ArticleComment.of(
+                Article.of(createUserAccount(), "title", "content", "hashtag"),
+                createUserAccount(),
+                content
+        );
+    }
+
+    private Member createUserAccount() {
+        Member member = new Member();
+        member.setMemberId(1L);
+        member.setEmail("test@test.com");
+        member.setPassword("TestPassword");
+        member.setNickname("Test User");
+
+        return member;
+    }
+
+    private Article createArticle() {
+        return Article.of(
+                createUserAccount(),
+                "title",
+                "content",
+                "#java"
+        );
+    }
 
 }
